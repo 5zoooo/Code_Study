100e34fb963b2d45786cf4cef101c25905bb17dc
wochae <officialwochae@gmail.com>
2025-04-01 20:08:54 +0900

    nm2

---
+++ Week6/채우석/Boj15650_2.java
@@ -0,0 +1,33 @@
+package Week6.채우석;
+
+import java.util.Scanner;
+
+//N과 M (2)
+public class Boj15650_2 {
+	static int N, M;
+	static int[] result;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+		result = new int[M];
+
+		comb(0, 1);
+	}
+
+	static void comb(int idx, int start) {
+		if (idx == M) {
+			for (int i = 0; i < result.length; i++) {
+				System.out.print(result[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = start; i <= N; i++) {
+			result[idx] = i;
+			comb(idx + 1, i + 1);
+		}
+	}
+}
