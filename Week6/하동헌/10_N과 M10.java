7642f19f0dc71968c0b054100967d33374313655
하동헌 <149583958+imclaremont@users.noreply.github.com>
2025-04-01 20:18:47 +0900

    복잡하다 복잡해~

---
+++ Week6/하동헌/10_N과 M10.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[] arr;
+
+    private static boolean[] visited;
+    private static int[] res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        res = new int[M];
+        visited = new boolean[N];
+        dfs(0, 0);
+
+        br.close();
+    }
+
+    private static void dfs(int at, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(res[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        int prevNum = -1;
+        for (int i = at; i < N; i++) {
+            if (!visited[i] && arr[i] != prevNum) {
+                visited[i] = true;
+                res[depth] = arr[i];
+                dfs(i, depth + 1);
+                visited[i] = false;
+                prevNum = arr[i];
+            }
+        }
+    }
+}
