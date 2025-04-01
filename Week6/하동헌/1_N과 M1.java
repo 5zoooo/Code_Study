b1550e7b0a4bb8c1bbe0eb0f3e26f5ac5810219f
하동헌 <149583958+imclaremont@users.noreply.github.com>
2025-04-01 10:35:19 +0900

    되돌아가는 과정 - 백트래킹

--- Week6/하동헌/1_N과 M1.java
+++ Week6/하동헌/1_N과 M1.java
@@ -1,13 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 // 백트래킹: 완전탐색 + 가지치기
-// 이 문제에서 가지치기는 없다(완탐만 있음)
+// 이 문제에서 가지치기는 없다! (완탐만 있음)
 public class Main {
     private static int N, M;
-    private static int[] arr;
+    private static int[] res;
     private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -17,7 +15,7 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];
+        res = new int[M];
         visited = new boolean[N + 1];
         dfs(0);
 
@@ -26,8 +24,8 @@ public class Main {
 
     private static void dfs(int depth) {
         if (depth == M) {
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
+            for (int i = 0; i < M; i++) {
+                System.out.print(res[i] + " ");
             }
             System.out.println();
             return;
@@ -36,9 +34,9 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                arr[depth] = i;
-                dfs(depth + 1);
-                visited[i] = false; // 자식노드 방문이 끝나고 돌아왔을 때 원상 복구
+                res[depth] = i;
+                dfs(depth + 1); 
+                visited[i] = false; // 자식노드 방문이 끝나고 돌아올 때 원상 복구
             }
         }
     }
