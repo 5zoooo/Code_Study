19deca4a9d4c562bccc0a591191e8a1f762fd7d7
하동헌 <149583958+imclaremont@users.noreply.github.com>
2025-04-01 19:12:57 +0900

    StringBuilder 안 쓰면 시초!

---
+++ Week6/하동헌/7_N과 M7.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[] arr;
+    private static int[] res;
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        res = new int[M];
+        sb = new StringBuilder();
+        dfs(0);
+
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(res[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            res[depth] = arr[i];
+            dfs(depth + 1);
+        }
+    }
+}
