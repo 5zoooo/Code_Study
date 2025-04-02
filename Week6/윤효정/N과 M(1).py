5ae0d54191ff333c10c259a9e9cf69a9e442da5b
Yoooohj <dbsgywjd@cau.ac.kr>
2025-04-02 16:16:04 +0900

    코드 추가 1

--- Week6/윤효정/N과 M(1).py
+++ Week6/윤효정/N과 M(1).py
@@ -8,3 +8,24 @@ for i in sorted(comb) :
     for j in i :
         print(j, end=' ')
     print()
+
+# 다른 방법
+n, m = map(int, input().split())
+cand = list(range(1, n+1))
+result = []
+tmp = []
+
+def bt():
+    if len(tmp) == m:
+        result.append(tmp[:])  # 복사본 저장
+        return
+    for i in cand:
+        if i not in tmp:
+            tmp.append(i)
+            bt()
+            tmp.pop()  
+
+bt()
+
+for a in result:
+    print(' '.join(map(str, a)))
--- Week6/윤효정/N과 M(4).py
+++ Week6/윤효정/N과 M(4).py
@@ -4,3 +4,25 @@ n, m = map(int, input().split())
 
 for c in combinations_with_replacement(range(1, n+1), m):
     print(' '.join(map(str, c)))
+
+
+#다른 방법
+n, m = map(int, input().split())
+cand = list(range(1, n+1))
+result = []
+tmp = []
+
+def bt():
+    if len(tmp) == m:
+        result.append(tmp[:])  # 복사본 저장
+        return
+    for i in cand:
+        if len(tmp) ==0 or i >= tmp[-1]:
+            tmp.append(i)
+            bt()
+            tmp.pop()  
+
+bt()
+
+for a in result:
+    print(' '.join(map(str, a)))
