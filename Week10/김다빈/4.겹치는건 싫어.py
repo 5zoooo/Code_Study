20eb17c8dcf11ffd242f664293a60751f72d8144
김다빈 <mokdb17@gmail.com>
2025-04-27 20:20:08 +0900

    4번이 제일 어려움

--- Week10/김다빈/4.겹치는건 싫어.py
+++ Week10/김다빈/4.겹치는건 싫어.py
@@ -1,10 +1,18 @@
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 arr = list(map(int, input().split()))
-numsDict = dict()
-start = 0
-end = len(arr) - 1
 
-"""
-나 이거 어캐품?
-꼭 이따가 풀어야지
-"""
+start = 0
+answer = 0
+numDict = dict()
+
+for i in range(len(arr)):
+    numDict[arr[i]] = numDict.get(arr[i], 0) + 1
+    if numDict[arr[i]] > k:
+        while numDict[arr[i]] > k:
+            numDict[arr[start]] -= 1
+            if numDict[arr[start]] == 0:
+                del numDict[arr[start]]
+            start += 1
+    answer = max(answer, i - (start - 1))
+
+print(answer)
